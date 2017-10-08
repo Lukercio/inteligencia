ebf53c01ff933f5ad652037acf617cb37ef51ea4
Lukercio <lukercio@live.com>
2017-10-08 12:51:55 -0300

    iniciando projeto

--- minimos-quadrados/src/MinimosQuadrados.java
+++ minimos-quadrados/src/MinimosQuadrados.java
@@ -3,6 +3,17 @@
  */
 public class MinimosQuadrados {
 
+    public double multiplicacaoMatriz(){
+        double r = 0;
 
+
+        return r;
+    }
+
+    public static void main(String []args){
+
+
+
+    }
 
 }
