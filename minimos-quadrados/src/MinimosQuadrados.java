0ac2351eb7cdf8a71269a6e71f6a7de19ff1f74e
Lukercio <lukercio@live.com>
2017-10-16 11:55:50 -0200

    comentarios iniciais

--- minimos-quadrados/src/MinimosQuadrados.java
+++ minimos-quadrados/src/MinimosQuadrados.java
@@ -5,26 +5,23 @@ import java.util.Scanner;
  */
 public class MinimosQuadrados {
 
+    // Limpa a tela da linha de comandos do Windows e Linux
     public static void limpaTela() {
-        try
-        {
+        try {
             final String os = System.getProperty("os.name");
 
-            if (os.contains("Windows"))
-            {
+            if (os.contains("Windows")) {
                 Runtime.getRuntime().exec("cls");
-            } else
-            {
+            } else {
                 Runtime.getRuntime().exec("clear");
             }
         }
-        catch (final Exception e)
-        {
-            //  Handle any exceptions.
+        catch (final Exception e) {
+            e.printStackTrace();
         }
     }
 
-
+    // Mostra uma mensagem e a matriz na tela formatada
     public static void exibeMatriz(int[][] m, int tamanho, String mensagem) {
         System.out.println("\n" + mensagem);
 
@@ -37,18 +34,27 @@ public class MinimosQuadrados {
         }
 
     }
-    public double multiplicacaoMatriz(){
+
+    // Multiplica Matrizes
+    public double multiplicaMatriz(){
         double r = 0;
 
 
         return r;
     }
 
+    // Calcula Determinante
+    public static double calculaDeterminante() {
+
+
+    }
+
+    // Executa comandos em sequencia
     public static void main(String []args){
         int entrada, i, j;
         Scanner n = new Scanner( System.in );
 
-
+        // Verifica o tamanho da matriz desejada
         do {
             limpaTela();
             System.out.println("Método dos Mínimos Quadrados\n\n");
@@ -60,6 +66,7 @@ public class MinimosQuadrados {
 
         System.out.println("Informe os valores da matriz " + entrada +" x " + entrada + " :");
 
+        // Le Matriz com tamanho informado anteriormente
         for (i=0; i < entrada; i++) {
             for(j = 0; j< entrada; j++) {
                 matriz[i][j] = n.nextInt();
