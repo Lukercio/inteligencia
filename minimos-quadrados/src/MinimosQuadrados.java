aecfcff3e407953be76648784ec40190057b28ed
Lukercio <lukercio@live.com>
2017-10-17 15:06:00 -0200

    height x shoe size exemplo

--- minimos-quadrados/src/MinimosQuadrados.java
+++ minimos-quadrados/src/MinimosQuadrados.java
@@ -215,7 +215,7 @@ public class MinimosQuadrados {
         double[][] matriz1 = { {1,69}, {1,67}, {1,71}, {1,65}, {1,72}, {1,68}, {1,74}, {1,65}, {1,66}, {1,72}} ;
         double[][] matriz2 = { {9.5}, {8.5}, {11.5}, {10.5}, {11}, {7.5},{12}, {7}, {7.5},{13}};
 
-        exibeMatriz(calculaMMQ(matriz1, matriz2, false), "Calculo MMQ");
+        exibeMatriz(calculaMMQ(matriz1, matriz2, false), "Calculo MMQ - height x shoe size");
 
 
     }
