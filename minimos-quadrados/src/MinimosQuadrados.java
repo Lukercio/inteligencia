89e89a230c8a9ab074cb1e24739c4f8834bc1915
Lukercio <lukercio@live.com>
2017-10-17 03:30:25 -0200

    metodos de operacoes com matrizes implementados

--- minimos-quadrados/src/MinimosQuadrados.java
+++ minimos-quadrados/src/MinimosQuadrados.java
@@ -209,8 +209,8 @@ public class MinimosQuadrados {
         exibeMatriz(calculaTransposta(matriz), "Matriz transposta:" );
 
         double[][] matriz1 = { {1,69}, {1,67}, {1,71}, {1,65}, {1,72}, {1,68}, {1,74}, {1,65}, {1,66}, {1,72}};
-        double[][] matriz2 = { {9.5}, {8.5}, {11.5}, {10.5}, {11}, {7.5}, {12}, {7}, {7.5}, {13} };
-
+        double[][] matriz2 = { {9.5, 8.5, 11.5, 10.5, 11, 7.5,12, 7, 7.5,13 }};
+exibeMatriz(multiplicaMatriz(matriz1,matriz2),"ulti");
         exibeMatriz(calculaMMQ(matriz1, matriz2, false), "Calculo MMQ");
 
 
